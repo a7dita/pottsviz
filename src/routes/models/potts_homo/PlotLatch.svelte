3e6a80d1e09579155cb60a93033f80f8c8fecbe3
sudraka <sudraka.69@gmail.com>
2023-10-19 23:59:21 +0200

    add a D3 plotting module for latching

---
+++ src/routes/models/potts_homo/PlotLatch.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+
+	interface DataPoint {
+		timeStep: number[];
+		values: number[][];
+	}
+
+	let data: DataPoint = { timeStep: [], values: [] };
+
+	// Fetch data
+	onMount(async () => {
+		const response = await fetch('../src/routes/data/mall_cue0_seed1_d');
+		const fileContent = await response.text();
+		const lines = fileContent.split('\n');
+
+		lines.forEach((line, i) => {
+			const parts = line.split('\t').map(Number);
+			data.timeStep[i] = parts[0];
+
+			parts.slice(1).forEach((value, j) => {
+				if (!data.values[j]) {
+					data.values[j] = [];
+				}
+				data.values[j][i] = value;
+			});
+		});
+	});
+
+	// FIXME debug the extra line of entry
+	$: {
+		if (data.timeStep.length && data.values.length) {
+			const svg = d3.select('#chart').attr('width', 650).attr('height', 500);
+
+			const x = d3
+				.scaleLinear()
+				.domain([0, Math.max(...data.timeStep)])
+				.range([0, 650]);
+			// FIXME set the width according to the time range
+
+			const y = d3.scaleLinear().domain([0, 1]).range([500, 0]);
+
+			data.values.forEach((lineData, j) => {
+				const line = d3
+					.line<number>()
+					.x((d, i) => x(data.timeStep[i]))
+					.y((d) => y(d));
+
+				svg
+					.append('path')
+					.datum(lineData)
+					.attr('d', line)
+					.attr('stroke-width', 1.2)
+					.attr('stroke', `hsl(${j * (360 / data.values.length)}, 100%, 50%)`) // Each line will have a unique hue
+					// .attr('stroke', d3.schemeCategory10[j])
+					.attr('fill', 'none');
+			});
+		}
+	}
+</script>
+
+<div class="">
+	<svg id="chart" />
+</div>
