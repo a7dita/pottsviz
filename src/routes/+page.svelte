53536b59099181a6f8b7e3270c6eaecb4e9ff81f
sudraka <sudraka.69@gmail.com>
2023-12-11 01:56:49 +0100

    Update route pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <!-- <div class="space-y-10"> -->
 <div class="space-y-16 flex flex-col place-items-center">
 	<div class="space-y-4">
-		<p class="text-purple">
-			This toy project will help us to visualize <i>latching</i> in <strong>Potts network</strong> in
-			real time.
+		<p class="text-purple text-2xl">
+			This toy project will help you to see memory <strong>latching</strong> dynamics in real time,
+		</p>
+		<p class="text-purple text-2xl">
+			in <strong>Potts associative network</strong> model of mammalian cortex.
 		</p>
 		<p class="text-lime">
-			Check <a href="https://doi.org/10.1080/02643290442000329" class="text-green-600">this paper</a
+			Check <a
+				href="https://doi.org/10.1080/02643290442000329"
+				class="text-lime-500 underline hover:text-blue-300">this pioneer paper</a
 			> to read more about the model.
 		</p>
 	</div>
@@ -18,7 +22,7 @@
                        hover:border-gray-300 hover:scale-110"
 			href="/models"
 		>
-			Let's go?
+			Let's go!
 		</a>
 	</div>
 </div>
